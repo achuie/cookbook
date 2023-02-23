4829db25be922ceb04f31cc984f788f8fe7aa20a
achuie <achuie@protonmail.com>
2023-02-23 23:00:04 +0900

    Read secrets from environment via decouple.

--- cookbookapp/cookbookapp/settings.py
+++ cookbookapp/cookbookapp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g%g049q!9ssh+o2l^=y2d7+u9)vp0rxcb9%!o#584r#vv8r48r'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
