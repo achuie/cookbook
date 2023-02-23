03a1173fc064b0f69745812e762004dc316ce973
achuie <achuie@protonmail.com>
2023-02-23 22:55:24 +0900

    Add env file for storing keys.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "DIY cookbook app";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -11,8 +11,18 @@
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShell = pkgs.mkShell {
-          buildInputs =
-            [ (pkgs.python3.withPackages (ps: with ps; [ django ])) ];
+          buildInputs = [
+            (pkgs.python3.withPackages
+              (ps: with ps; [ django python-decouple ]))
+          ];
+          shellHook = ''
+            if [ ! -f "./cookbookapp/.env" ]; then
+              echo '  `.env` not found so generating secret key.'
+              echo "SECRET_KEY="$(python -c 'from django.core.management import utils; print(utils.get_random_secret_key())') >./cookbookapp/.env
+            else
+              echo '  Found existing `.env`.'
+            fi
+          '';
         };
 
         formatter = pkgs.nixfmt;
