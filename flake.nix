71de68e839ae1e6a0a67ce0947887a29353cd9af
achuie <achuie@protonmail.com>
2023-04-22 17:28:41 +0900

    Don't depend on flake-utils; start dev shell in app directory.

--- flake.nix
+++ flake.nix
@@ -1,30 +1,37 @@
 {
   description = "DIY cookbook app";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            (pkgs.python3.withPackages
-              (ps: with ps; [ django python-decouple ]))
-          ];
-          shellHook = ''
-            if [ ! -f "./cookbookapp/.env" ]; then
-              echo '  `.env` not found so generating secret key.'
-              echo "SECRET_KEY="$(python -c 'from django.core.management import utils; print(utils.get_random_secret_key())') >./cookbookapp/.env
-            else
-              echo '  Found existing `.env`.'
-            fi
-          '';
-        };
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              (pkgs.python3.withPackages
+                (ps: with ps; [ django python-decouple ]))
+            ];
+            shellHook = ''
+              if [ ! -f "./cookbookapp/.env" ]; then
+                echo "  '.env' not found so generating secret key."
+                echo "SECRET_KEY="$(python -c 'from django.core.management import utils; print(utils.get_random_secret_key())') >./cookbookapp/.env
+              else
+                echo "  Found existing '.env'."
+              fi
+              cd cookbookapp
+              echo "  Now in app dir: '$(pwd)'"
+            '';
+          };
+        });
 
-        formatter = pkgs.nixfmt;
-      });
+      formatter = forAllSystems (system: nixpkgsFor.${system}.nixpkgs-fmt);
+    };
 }
