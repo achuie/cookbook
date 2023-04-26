c3ca59600602ea3274a5426f781dc5ea74338a7a
achuie <achuie@protonmail.com>
2023-04-26 21:25:44 +0900

    Rename project for @jilaada.

--- cookbooksite/cookbooksite/asgi.py
+++ cookbooksite/cookbooksite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for cookbookapp project.
+ASGI config for cookbooksite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cookbookapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cookbooksite.settings')
 
 application = get_asgi_application()
--- cookbooksite/cookbooksite/wsgi.py
+++ cookbooksite/cookbooksite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cookbookapp project.
+WSGI config for cookbooksite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cookbookapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cookbooksite.settings')
 
 application = get_wsgi_application()
