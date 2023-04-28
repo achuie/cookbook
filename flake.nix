1eac5903dd39758b154db40c5ae2a1bbc222a55f
achuie <31836492+achuie@users.noreply.github.com>
2023-04-28 20:55:55 +0900

    Hello world (#3)
    * Add nix flake for django environment.
    
    * Ignore files from nix and django.
    
    * Add initial hello-world site.
    
    * Add env file for storing keys.
    
    * Read secrets from environment via decouple.
    
    * Don't depend on flake-utils; start dev shell in app directory.
    
    * Rename project for @jilaada.
    
    * Update dev env.

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "DIY cookbook app";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              (pkgs.python3.withPackages
+                (ps: with ps; [ django python-decouple ]))
+            ];
+            shellHook = ''
+              if [ ! -f "./cookbooksite/.env" ]; then
+                echo "  '.env' not found so generating secret key."
+                echo "SECRET_KEY="$(python -c 'from django.core.management import utils; print(utils.get_random_secret_key())') >./cookbooksite/.env
+              else
+                echo "  Found existing '.env'."
+              fi
+              cd cookbooksite
+              echo "  Now in app dir: '$(pwd)'"
+            '';
+          };
+        });
+
+      formatter = forAllSystems (system: nixpkgsFor.${system}.nixpkgs-fmt);
+    };
+}
